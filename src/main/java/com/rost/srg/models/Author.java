314335cc3eca4a2600f2b662991b378ac48e4ae3
Rost <coderost@yandex.ru>
2023-01-15 10:57:37 +0300

    Create entity class for author

---
+++ src/main/java/com/rost/srg/models/Author.java
@@ -0,0 +1,30 @@
+package com.rost.srg.models;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "author")
+    private List<Article> articles;
+}
