d1cd11a32b7150dc0145d1458501bbe3951260bd
Rost <coderost@yandex.ru>
2023-01-15 20:13:43 +0300

    Add starting of TCP server for access from third-party apps.

--- src/main/java/com/rost/srg/Application.java
+++ src/main/java/com/rost/srg/Application.java
@@ -1,13 +1,20 @@
 package com.rost.srg;
 
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server inMemoryH2DatabaseaServer() throws SQLException {
+		return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
+	}
 }
